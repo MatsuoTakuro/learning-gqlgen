144415cb3e39eb4817f4eadcf5e1d5cecd8afce1
MatsuoTakuro <matsuo_takuro@retail-ai.jp>
2022-03-28 00:00:24 +0900

    Update server starter

--- go.mod
+++ go.mod
@@ -4,7 +4,6 @@ go 1.17
 
 require (
 	github.com/99designs/gqlgen v0.17.2
-	github.com/99designs/gqlgen/_examples v0.0.0-20220323140303-36fb3dc67336
 	github.com/mitchellh/mapstructure v1.4.1
 	github.com/vektah/gqlparser/v2 v2.4.0
 )
@@ -13,4 +12,5 @@ require (
 	github.com/agnivade/levenshtein v1.1.0 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/hashicorp/golang-lru v0.5.0 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
 )
--- server/server.go
+++ server/server.go
@@ -5,15 +5,25 @@ import (
 	"errors"
 	"log"
 	"net/http"
+	"os"
 	"runtime/debug"
 
-	"github.com/99designs/gqlgen/_examples/todo"
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/MatsuoTakuro/learning-gqlgen/graph/generated"
+	"github.com/MatsuoTakuro/learning-gqlgen/graph/resolver"
 )
 
+const defaultPort = "8085"
+const title = "gqlgen-todo"
+
 func main() {
-	srv := handler.NewDefaultServer(todo.NewExecutableSchema(todo.New()))
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(resolver.New()))
 	srv.SetRecoverFunc(func(ctx context.Context, err interface{}) (userMessage error) {
 		// send this panic somewhere
 		log.Print(err)
@@ -21,7 +31,9 @@ func main() {
 		return errors.New("user message on panic")
 	})
 
-	http.Handle("/", playground.Handler("gqlgen-todo", "/query"))
+	http.Handle("/", playground.Handler(title, "/query"))
 	http.Handle("/query", srv)
-	log.Fatal(http.ListenAndServe(":8081", nil))
+
+	log.Printf("connect to http://localhost:%s/ for %s", port, title)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
